82d096e5164607207c1ce143bad822eeec4b4219
Maria João <mariajoao.tlp@gmail.com>
2022-07-14 07:18:56 +0100

    Edit component Home

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -4,8 +4,11 @@ const Home = () => {
     
     return (
         <>
-            <div className='Home'>
-                <h1>Home</h1>
+            <div className='Home container h-100 py-5'>
+                <div className='ratio ratio-16x9 video my-5'>
+                    <iframe width='640' height='360' src='https://www.youtube.com/embed/n8D_fLiFQ7o' title='O Início' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>
+                </div>
+                <p className='text-center text-white text-opacity-50'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis voluptas molestiae dolorem a quaerat natus ut, ipsa odit nisi illum autem reprehenderit ad eaque! Autem tempora nobis aut laboriosam eum?</p>
             </div> 
         </>
     )
